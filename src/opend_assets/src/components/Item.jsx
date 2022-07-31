03287db6564b7527518210137be4062852c35823
vineet chopra <vineetchopra0007@gmail.com>
2022-07-31 23:55:18 +0530

    Initial commit

---
+++ src/opend_assets/src/components/Item.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/logo.png";
+import {Actor , HttpAgent} from "@dfinity/agent";
+import { idlFactory } from "../../../declarations/nft";
+import {Principal} from "@dfinity/principal";
+
+function Item(props) {
+
+  const [name,setName] = useState();
+
+  const id = Principal.fromText(props.id);
+
+  const localhost = "http://localhost:8080/";
+  const agent = new HttpAgent({host:localhost});
+
+  async function loadNFT(){
+    const NFTActor = await Actor.createActor(idlFactory,{
+      agent,
+      canisterId:id,
+    });
+    const name = await NFTActor.getName();
+    setName(name);
+  }
+
+  useEffect(()=>{
+    loadNFT();
+  },[]);
+
+
+  return (
+    <div className="disGrid-item">
+      <div className="disPaper-root disCard-root makeStyles-root-17 disPaper-elevation1 disPaper-rounded">
+        <img
+          className="disCardMedia-root makeStyles-image-19 disCardMedia-media disCardMedia-img"
+          src={logo}
+        />
+        <div className="disCardContent-root">
+          <h2 className="disTypography-root makeStyles-bodyText-24 disTypography-h5 disTypography-gutterBottom">
+            {name}<span className="purple-text"></span>
+          </h2>
+          <p className="disTypography-root makeStyles-bodyText-24 disTypography-body2 disTypography-colorTextSecondary">
+            Owner: sdfsdf-erwerv-sdf
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Item;
